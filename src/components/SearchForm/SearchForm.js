6310f4abf0fa2d5cd4f82eb0272a3dc293e9d15b
Waldemar Szymon <walldy@tlen.pl>
2023-02-16 14:09:17 +0100

    Create application

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,12 @@
+import styles from './SearchForm.module.scss';
+
+const SearchForm = () => {
+  return (
+    <form className={styles.searchForm}>
+      <input className={styles.input} type="text" />
+      <button className={styles.button}>Search</button>
+    </form>
+  );
+};
+
+export default SearchForm;
