b6d4aaed85dc17d91da103eea69dd0e5dd5ac9dd
Waldemar Szymon <walldy@tlen.pl>
2023-02-16 17:04:23 +0100

    Change Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import styles from './Button.module.scss';
 
 const Button = props => {
   return (
-    <button className={styles.button}>{props.text}</button>
+    <button className={styles.button}>{props.children}</button>
   );
 };
 
