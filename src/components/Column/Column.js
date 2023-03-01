ec27fbd04f4c5d74ebda99e25f4b8c3bdc23c6da
Waldemar Szymon <walldy@tlen.pl>
2023-03-01 13:21:13 +0100

    [kodilla task 17.2] Use Redux library

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,14 +1,18 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
   return (
     <article className={styles.column}>
       <span className={styles.icon + ' fa fa-' + props.icon} />
       <h2 className={styles.title}>{props.title}</h2>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} name={card.title} />)}
+        {cards.map(card => <Card key={card.id} name={card.title} />)}
       </ul>
       <CardForm columnId={props.id} action={props.action} />
     </article>
