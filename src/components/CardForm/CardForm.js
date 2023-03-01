45b637d7cd8e2bbf609b414b72aad10e5f7beb56
Waldemar Szymon <walldy@tlen.pl>
2023-03-01 14:47:00 +0100

    [kodilla task 17.3] Add actions in reducer

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,13 +2,16 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
+
   const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.action({ title: title }, props.columnId);
+    dispatch({ type: 'ADD_CARD', payload: { title: title,  columnId: props.columnId } });
     setTitle('');
   };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
+import shortid from 'shortid';
 
 const reducer = (state, action) => {
-  return state;
+
+  switch (action.type) {
+
+    case 'ADD_COLUMN':
+      return { ...state, columns: [...state.columns, { id: shortid(), ...action.payload }]};
+
+    case 'ADD_CARD':
+      return { ...state, cards: [...state.cards, { id: shortid(), ...action.payload }] };
+    
+      default:
+        return state;
+
+  }
 };
 
 const store = createStore(
